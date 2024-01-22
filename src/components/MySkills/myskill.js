6c221846cfcd22c0b7390326daaa6d21ea0831d9
dananjaya <dahamdv@gmail.com>
2024-01-22 09:56:05 +0530

    add new images and description

--- src/components/MySkills/myskill.js
+++ src/components/MySkills/myskill.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import './myskill.css';
+import Cimage from '../../assets/C#img.jpg';
+import Astnet from '../../assets/asp.netimg.png';
+
 
 const Myskill = () => {
   return (
-    <div>myskill</div>
+    <section id='myskills'>
+<span className='skillTiitle'> What are my Skills </span>
+
+
+            <div className='skillBar'>
+                <img src={Cimage} alt="Cimage" className='skillBarImg'/>
+                <div className='skillBarText'> 
+                <h2>C# Language</h2>
+                <p>I have more then 2years experiance abount C# and i have on going project</p>
+                    </div>
+
+            </div>
+
+            <div className='skillBar'>
+                <img src={Astnet} alt="Astnet" className='skillBarImg'/>
+                <div className='skillBarText'> 
+                <h2>ASP.NET Language</h2>
+                <p>i Have more then 1 year experiance about the ASP.NET </p>
+                    </div>
+
+            </div>
+        
+    </section>
+
+
+    
   )
 }
 
